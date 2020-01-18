b554984f62fe0c491aaf878db416d66448e2f0d8
Po4aPo <suhar06.12.00@gmail.com>
2020-01-18 21:49:20 +0400

    test commit

--- Project_3_First_Game/main.py
+++ Project_3_First_Game/main.py
@@ -28,6 +28,30 @@ class Ball:
             self.x = -3
 
 
+class Paddle:
+    def __init__(self, canvas, color):
+        self.canvas = canvas
+        self.id = canvas.create_rectangle(0,0,100,10,fill=color)
+        self.canvas.move(self.id, 200,300,)
+        self.x = 0
+        self.canvas_width = self.canvas.winfo_width()
+        self.canvas.bind_all("<KeyPress-Left>", self.turn_left)
+        self.canvas.bind_all("<KeyPress-Right>", self.turn_right)
+
+    def turn_left(self, evt):
+        self.x = -2
+
+    def turn_right(self, evt):
+        self.x = 2
+
+    def draw(self):
+        self.canvas.move(self.id,self.x,0)
+        pos = self.canvas.coords(self.id)
+        if pos[0] <= 0:
+            self.x = 0
+        elif pos[2] >= self.canvas_width:
+            self.x = 0
+
 
 tk =Tk()
 tk.title("Игра")
@@ -38,9 +62,11 @@ canvas.pack()
 tk.update()
 
 ball = Ball(canvas,"red")
+paddle = Paddle(canvas, 'blue')
 
 while 1:
+    paddle.draw()
     ball.draw()
     tk.update_idletasks()
     tk.update()
-    time.sleep(0.025)
+    time.sleep(0.01)
